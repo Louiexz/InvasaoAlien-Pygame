35089d8f2f4b1df651d26b98b3568035244d4c09
Louiexz <fluizlucas@gmail.com>
2024-03-19 21:37:10 -0300

    Refatorando código + sons

--- start.py
+++ start.py
@@ -1,27 +1,27 @@
 import pygame as pyg
 from pygame.sprite import Group
 import character as characters
-import settings as sets
-import game_functions as g_f
+import settings as setts
+from button import Button
+from game_functions import GameLoopManager as loop
 
-def initialize_game():
-	# Inicializa o jogo e cria um objeto para a tela
-	pyg.init()
-	pyg.display.set_caption("Alien Invasion")
-	# Obtém a resolução da tela do dispositivo
-	screen_info = pyg.display.Info()
-	# Cria a tela com base na resolução do dispositivo
-	width = screen_info.current_w - 50
-	height = screen_info.current_h - 50
-	screen = pyg.display.set_mode((width, height))
-	ai_sets = sets.Settings(width, height, screen)
-	# Cria uma espaçonave
-	character = characters.Character(screen, 'img/new_ship2', ai_sets)
-	# Cria um grupo no qual serão armazernados os projéteis e aliens
-	bullets = Group()
-	# Cria um alienígena
-	aliens = Group()
-	# Inicia o laço principal do jogo
-	g_f.game_loop(aliens, bullets, character, ai_sets, screen)
+class InitializeGame:
+    def __init__(self):
+        pyg.init()
+        pyg.display.set_caption("Alien Invasion")
 
-initialize_game()
+        screen_info = pyg.display.Info()
+        width = screen_info.current_w - 50
+        height = screen_info.current_h - 50
+        screen = pyg.display.set_mode((width, height))
+
+        ai_sets = setts.Settings(width, height, screen)
+        character = characters.Character(screen, 'img/new_ship2', ai_sets)
+        buttons = [Button(pyg, *button) for button in ai_sets.buttons]
+
+        bullets = Group()
+        aliens = Group()
+
+        loop.run_game_loop(screen, character, bullets, ai_sets, aliens, buttons)
+
+if __name__ == "__main__": InitializeGame()
