8cfb080e5c56fd22b8ddc15d51a927801444f781
Louiexz <fluizlucas@gmail.com>
2024-03-29 22:18:22 -0300

    Ajustando códigos

--- settings.py
+++ settings.py
@@ -8,14 +8,14 @@ class Settings():
 		self.screen_height = screen_height
 		self.bg_color = (100, 100, 100)
 		self.espaco = (0, 0, 33)
-		self.ceu= (135, 206, 235)
+		self.ceu = (135, 206, 235)
 		# Levels
 		self.stage = 0
 		# Configurações dos projéteis
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = 60, 60, 60
-		self.bullet_speed_factor = 15
+		self.bullet_speed_factor = 10
 		self.bullets_allowed = 3
 		# Configurações da espaçonave
 		self.ship_speed_factor = 3
@@ -33,14 +33,17 @@ class Settings():
 		button3_rect = [20, screen_height * 0.25, 140, 40, (0, 0, 0), "Stop/Rerun"]
 
 		self.buttons = [button1_rect, button2_rect, button3_rect]
-
 		# Loop
 		self.pause = False
 		self.rodando = True
 
-	def restart():
-		self.alien_speed_factor = 1
+	def restart(self):
+		self.bg_color = (100, 100, 100)
+		self.alien_speed_factor = 5
 		self.bullets_allowed = 3
-		self.bg_color = True
-		self.fleet_drop_speed = 40
-		self.ship_speed_factor = 5
+		self.bullet_speed_factor = 15
+		self.fleet_drop_speed = 50
+		self.ship_speed_factor = 3
+		self.stage = 0
+		self.count = 0
+		self.ship_lifes = 5
